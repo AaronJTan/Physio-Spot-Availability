7ce7e69ac64d486c0d5cba6b2e79571a81694ef5
Aaron Tan <aaronjtan00@gmail.com>
2023-08-15 11:21:50 -0400

    add: physiotherapists config file

---
+++ config/physiotherapists.js
@@ -0,0 +1,27 @@
+export const physiotherapists = [
+    {
+        id: "116098",
+        name: "Saro Minassian",
+        qualifications: "Registered Physiotherapist, PT, MScPT, BAH Kin"
+    },
+    {
+        id: "153291",
+        name: "Malcolm Loukides",
+        qualifications: "Registered Physiotherapist, MScPT, BSc. Hons."
+    },
+    {
+        id: "964370361925764739",
+        name: "Catherine Law",
+        qualifications: "Registered Physiotherapist, Pelvic Health Provider"
+    },
+    {
+        id: "939396707219670463",
+        name: "Shiqing (Sally) Chen",
+        qualifications: "Registered Physiotherapist with the College of Physiotherapists of Ontario, MScPT, BKin"
+    },
+    {
+        id: "1151374701386270170",
+        name: "Sara Jahangirizadeh",
+        qualifications: "PT Resident; MSc, BSc.; PT Supervisor Viacheslav Feldman, PT# 15020"
+    }
+]
